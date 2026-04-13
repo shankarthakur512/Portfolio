945792bb5a8ee281697335bbed8081912e2e3bc5
Shankar Thakur <109839045+shankarthakur512@users.noreply.github.com>
2026-04-13 12:41:02 +0530

    Update personal data description and resume link

--- utils/data/personal-data.js
+++ utils/data/personal-data.js
@@ -2,7 +2,7 @@ export const personalData = {
   name: "Shankar Thakur",
   profile: '/profile.png',
   designation: "Full stack Developer",
-  description: "My name is Shankar Thakur. I am a professional and enthusiastic programmer in my daily life. I am a quick learner with a self-learning attitude. I love to learn and explore new technologies and am passionate about problem-solving. I love almost all the stacks of web application development and love to make the web more open to the world. My core skill is based on JavaScript and I love to do most of the things using JavaScript. I am available for any kind of job opportunity that suits my skills and interests.",
+  description: "My name is Shankar Thakur. I am Software Engineer1 at Quark and a professional , enthusiastic programmer in my daily life. I am a quick learner with a self-learning attitude. I love to learn and explore new technologies and am passionate about problem-solving. I love almost all the stacks of web application development and love to make the web more open to the world. My core skill is based on JavaScript and I love to do most of the things using JavaScript. I am available for any kind of job opportunity that suits my skills and interests.",
   email: 'skt48324@gmail.com',
   phone: '+918409941552',
   address: 'Madhubani, Bihar, India - 847228 ',
@@ -13,5 +13,5 @@ export const personalData = {
   stackOverflow: 'https://stackoverflow.com/',
   leetcode: "https://leetcode.com/u/shankar__thakur/",
   devUsername: "Shankar_thakur",
-  resume: "https://drive.google.com/file/d/1yt-VmAwoO-HkGgY3-WBpa97bVUJCleV-/view?usp=sharing"
+  resume: "https://drive.google.com/file/d/1zjC9BUgsMvdg6tEpflKF0K6QuwB_8AFD/view?usp=drive_link
 }
